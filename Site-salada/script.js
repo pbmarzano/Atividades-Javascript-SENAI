4ef3d173b8111f84a02929d2e895e01eabd582b0
Pedro Marzano <musicomarzano@gmail.com>
2020-08-12 10:30:37 -0300

    refatoração do codigo salada de fruta

--- Site-salada/script.js
+++ Site-salada/script.js
@@ -2,12 +2,22 @@ var $lista = document.querySelector('#lista')
 var $fruta = document.querySelector('#fruta')
 var $botao = document.querySelector('#btn')
 
-$botao.addEventListener('click', function(){
+$fruta.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("btn").click();
+  }
+});
+
+function add(){
   var item = '<li>' + $fruta.value + '</li>'
-  $lista.innerHTML = $lista.innerHTML + item 
+  $lista.innerHTML += item
 
   if ($fruta.value == 'cereja'){
     alert('sua salada está pronta')
   }
-})
+
+  $fruta.value = '';
+  $fruta.focus()
+}
 
