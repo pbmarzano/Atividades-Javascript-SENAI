01491996a1f260135986939c8fd851b313322247
Pedro Marzano <musicomarzano@gmail.com>
2020-08-12 10:45:11 -0300

    changing archives

---
+++ Exercícios/script-sem-html/altura-pedrinho/script.js
@@ -0,0 +1,9 @@
+let alturaj = 175
+let alturap = 160
+
+for (anos = 0; alturaj > alturap; ++anos) {
+    alturaj ++
+    alturap += 3
+}
+
+console.log(`pedrinho levou ${anos} anos para ser mais alto que joao`)
